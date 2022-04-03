9c87784c3f0752e2600d29523ff89a3808336862
David Ka <david.karnel@student.kyh.se>
2022-04-03 15:40:51 +0200

    updated w. html & controller

---
+++ src/main/java/com/mycompany/box/BoxController.java
@@ -0,0 +1,25 @@
+package com.mycompany.box;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.util.List;
+
+@Controller
+public class BoxController {
+
+    @Autowired
+    private BoxService service;
+
+    @GetMapping("/boxes")
+        public String showAllBoxes (Model model){
+        List<Box> listBoxes = service.listAll();
+        model.addAttribute("listBoxes",listBoxes);
+
+        return "boxes";
+    }
+}
---
+++ src/main/java/com/mycompany/box/BoxService.java
@@ -0,0 +1,16 @@
+package com.mycompany.box;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoxService {
+
+    @Autowired private BoxRepository repo;
+
+    public List<Box> listAll(){
+        return (List<Box>) repo.findAll();
+    }
+}
